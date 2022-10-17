598ded13057ea37bddbc26d7c4f56c049a15d3a9
Johannes Raggam <thetetet@gmail.com>
2022-10-17 09:07:54 +0200

    fix: Fix initialization of Module Federation. This is done by the plone main/host bundle.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Webpack entry point for module federation.
-import "@patternslib/patternslib/webpack/module_federation";
 // The next import needs to be kept with brackets, otherwise we get this error:
 // "Shared module is not available for eager consumption."
 import("./bundle-config");
